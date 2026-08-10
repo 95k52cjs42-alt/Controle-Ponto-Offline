66f26e456c9bd386286e704605e5dab412f53cfc
Fourzito <amigosnerdemapuros@gmail.com>
2026-08-10 16:18:49 -0300

    Ultima teste

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from flask_login import (
     login_user,
     logout_user,
 )
+
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 
